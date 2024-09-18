ad8bdc91249fba2f2ef468c287dfe24964a3940f
zimward <96021122+zimward@users.noreply.github.com>
2024-09-19 02:53:03 +0900

    update cargoHash

--- package.nix
+++ package.nix
@@ -3,5 +3,5 @@ rustPlatform.buildRustPackage {
   version = "0.1.0";
   pname = "imgserv";
   src = ./.;
-  cargoHash = "sha256-CJWSfEr2QJfstUBB8kLOcITBG9yRA5vzPdhY05o6s64=";
+  cargoHash = "sha256-7xbPwsFzede+evb+TIh/F7lyuAeWMl0JR3nA2jPyArk=";
 }
